e8182fb1bbee98cf9cccfe099562e9c6af82ff4b
Toovuu <36646476+Toovuu@users.noreply.github.com>
2020-11-03 17:03:20 +0100

    Initial data for Krusty project

---
+++ initial-data.sql
@@ -0,0 +1,73 @@
+USE Krusty;
+
+INSERT INTO Customers(Customer_Name, Address) VALUES
+('Bjudkakor AB', 'Ystad'),
+('Finkakor AB', 'Helsingborg'),
+('Gästkakor AB', 'Hässleholm'),
+('Kaffebröd AB', 'Landskrona'),
+('Kalaskakor AB', 'Trelleborg'),
+('Partykakor AB', 'Kristianstad'),
+('Skånekakor AB', 'Perstorp'),
+('Småbröd AB', 'Malmö');
+
+INSERT INTO Products(Product_Name) VALUES
+('Almond delight'),
+('Amneris'),
+('Berliner'),
+('Nut cookie'),
+('Nut ring'),
+('Tango');
+
+INSERT INTO Ingredients(Ingredient_Name, Amount, Unit) VALUES
+('Bread crumbs', 500000, 'g'),
+('Butter', 500000, 'g'),
+('Chocolate', 500000, 'g'),
+('Chopped almonds', 500000, 'g'),
+('Cinnamon', 500000, 'g'),
+('Egg whites', 500000, 'ml'),
+('Eggs', 500000, 'g'),
+('Fine-ground nuts', 500000, 'g'),
+('Flour', 500000, 'g'),
+('Ground, roasted nuts', 500000, 'g'),
+('Icing sugar', 500000, 'g'),
+('Marzipan', 500000, 'g'),
+('Potato starch', 500000, 'g'),
+('Roasted, chopped nuts', 500000, 'g'),
+('Sodium bicarbonate', 500000, 'g'),
+('Sugar', 500000, 'g'),
+('Vanilla sugar', 500000, 'g'),
+('Vanilla', 500000, 'g'),
+('Wheat flour', 500000, 'g');
+
+INSERT INTO Recipes(Product_Name, Ingredient_Name, Amount) VALUES
+('Almond delight', 'Butter', 400),
+('Almond delight', 'Chopped almonds', 279),
+('Almond delight', 'Cinnamon', 10),
+('Almond delight', 'Flour', 400),
+('Almond delight', 'Sugar', 270),
+('Amneris', 'Butter', 250),
+('Amneris', 'Eggs', 250),
+('Amneris', 'Marzipan', 750),
+('Amneris', 'Potato starch', 25),
+('Amneris', 'Wheat flour', 25),
+('Berliner', 'Butter', 250),
+('Berliner', 'Chocolate', 50),
+('Berliner', 'Eggs', 50),
+('Berliner', 'Flour', 350),
+('Berliner', 'Icing sugar', 100),
+('Berliner', 'Vanilla sugar', 5),
+('Nut cookie', 'Bread crumbs', 125),
+('Nut cookie', 'Chocolate', 50),
+('Nut cookie', 'Egg whites', 350),
+('Nut cookie', 'Fine-ground nuts', 750),
+('Nut cookie', 'Ground, roasted nuts', 625),
+('Nut cookie', 'Sugar', 375),
+('Nut ring', 'Butter', 450),
+('Nut ring', 'Flour', 450),
+('Nut ring', 'Icing sugar', 190),
+('Nut ring', 'Roasted, chopped nuts', 225),
+('Tango', 'Butter', 200),
+('Tango', 'Flour', 300),
+('Tango', 'Sodium bicarbonate', 4),
+('Tango', 'Sugar', 250),
+('Tango', 'Vanilla', 2);
